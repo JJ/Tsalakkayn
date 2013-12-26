fa4c76538a3a64fc3060fc403f0d5a676f20529f
JJ Merelo <jjmerelo@gmail.com>
2013-12-26 11:21:52 +0100

    Resuelve problema acentos

--- parte.pl
+++ parte.pl
@@ -2,6 +2,7 @@
 
 use strict;
 use warnings;
+use utf8;
 
 use File::Slurp qw( read_file write_file);
 use v5.12;
@@ -10,11 +11,10 @@ my $file_name = shift || "Tsalakkayn.md";
 
 my $file = read_file($file_name) || die "No puedo abrir $file_name por $!\n";
 
-my $puntuacion = qr/[:;,.\(\)\?\!]/;
+my $puntuacion = qr/[^\w\s]\s+/;
 
 $file =~ s/\s+/ /g;
-$file =~ s/[-_*»¡¿]//g;
-$file =~ s/[^\x00-\xFF]//g; # Sugerido por http://stackoverflow.com/questions/10437455/perl-stop-print-if-wide-characters-error, elimina interrogaciones y eso
+$file =~ s/[-_*»¡¿«)(]//g;
 my @frases = split($puntuacion, $file );
 
 my %frecuencias;
@@ -30,9 +30,10 @@ my ( $name, $ext) = split(/\./, $file_name);
 open my $frecuencias_fh, ">", "$name.frecuencias.dat";
 open my $palabras_fh, ">", "$name.palabras.dat";
 open my $nombres_fh, ">", "$name.nombres.dat";
+
 for my $p ( sort { $frecuencias{$b} <=> $frecuencias{$a} } keys %frecuencias ) {
     say $frecuencias_fh "$p => $frecuencias{$p}";
-    say $nombres_fh $p if $p !~ /^[a-z]/;
+    say $nombres_fh $p if $p !~ /^[a-záéíóú]/;
     say $palabras_fh $p;
 }
 close $frecuencias_fh;
